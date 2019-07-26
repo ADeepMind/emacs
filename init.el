23e32865a9896ec237cbb7cb828eb1c964193041
ADeepMind <x1773778024@gmail.com>
2019-07-26 20:41:19 +0800

    index

--- init.el
+++ init.el
@@ -12,6 +12,17 @@
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
 
+
+;; org mode running source block
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (C . t)
+   (python . t)
+   (shell . t)
+   (latex . t)
+   (ditaa . t)))
+
 (menu-bar-mode 0)
 (show-paren-mode 1)
 (electric-pair-mode 1)
@@ -21,7 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (slime))))
+ '(package-selected-packages (quote (htmlize yasnippet slime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
