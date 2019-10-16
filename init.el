eb9c1c0603148586cba2c048f0c0d99dd69d2a6c
Jokii-Joy <x1773778024@gmail.com>
2019-10-16 18:01:13 +0800

    ss

--- init.el
+++ init.el
@@ -94,8 +94,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(company-tabnine-use-native-json nil)
- '(custom-enabled-themes (quote (adwaita)))
+ '(custom-enabled-themes (quote (tsdh-light)))
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
